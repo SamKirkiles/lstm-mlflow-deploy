ab2e5387b402689322c75ab9ba259011e52381fd
SamKirkiles <sam.kirkiles@gmail.com>
2018-04-10 20:08:59 -0400

    added mutli gpu and compute on cpu

--- lstm.py
+++ lstm.py
@@ -35,39 +35,41 @@ class LSTM:
 		self.ix_to_char = {i:ch for i,ch in enumerate(chars)}
 
 		# Define graph (This is one lstm cell but we want multiple cells)
-		with tf.device("/gpu:0"):
+		for d in ['/gpu:0', '/gpu:1']:
 
-			with tf.name_scope("hidden_states"):
+			with tf.device(d):
 
-				h_prev_placeholder = tf.placeholder(shape=[self.hidden_size,1],dtype=tf.float32,name="h_prev")
-				c_prev_placeholder = tf.placeholder(shape=[self.hidden_size,1],dtype=tf.float32,name="c_prev")
-				hidden_states = tf.stack([h_prev_placeholder,c_prev_placeholder])
+				with tf.name_scope("hidden_states"):
 
-			inputs_placeholder = tf.placeholder(shape=[seq_length,self.vocab_size],dtype=tf.float32,name="batch")
-			labels_placeholder = tf.placeholder(shape=[seq_length,self.vocab_size],dtype=tf.float32,name="batch")
+					h_prev_placeholder = tf.placeholder(shape=[self.hidden_size,1],dtype=tf.float32,name="h_prev")
+					c_prev_placeholder = tf.placeholder(shape=[self.hidden_size,1],dtype=tf.float32,name="c_prev")
+					hidden_states = tf.stack([h_prev_placeholder,c_prev_placeholder])
 
-			# this will be [25,2,vocab_size]
-			batch = tf.scan(self.lstm_cell,inputs_placeholder,initializer=hidden_states)
-			h_outputs,c_outputs = tf.unstack(batch,axis=1)
+				inputs_placeholder = tf.placeholder(shape=[seq_length,self.vocab_size],dtype=tf.float32,name="batch")
+				labels_placeholder = tf.placeholder(shape=[seq_length,self.vocab_size],dtype=tf.float32,name="batch")
+
+				# this will be [25,2,vocab_size]
+				batch = tf.scan(self.lstm_cell,inputs_placeholder,initializer=hidden_states)
+				h_outputs,c_outputs = tf.unstack(batch,axis=1)
 
 
-			Wout = tf.get_variable(name="Wout",shape=[self.vocab_size,self.hidden_size],dtype=tf.float32)
+				Wout = tf.get_variable(name="Wout",shape=[self.vocab_size,self.hidden_size],dtype=tf.float32)
 
-			h_outputs = tf.squeeze(h_outputs,axis=2)
+				h_outputs = tf.squeeze(h_outputs,axis=2)
 
-			h_new = tf.transpose(tf.matmul(Wout, tf.transpose(h_outputs)))
+				h_new = tf.transpose(tf.matmul(Wout, tf.transpose(h_outputs)))
 
-			softmax = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels_placeholder,logits=h_new)
-			
-			loss = tf.reduce_mean(softmax)
-			optimize = tf.train.AdagradOptimizer(0.1).minimize(loss)
+				softmax = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels_placeholder,logits=h_new)
+				
+				loss = tf.reduce_mean(softmax)
+				optimize = tf.train.AdagradOptimizer(0.1).minimize(loss)
 
-			hidden_state = tf.unstack(batch,axis=1)
-			# now compute loss and what not
+				hidden_state = tf.unstack(batch,axis=1)
+				# now compute loss and what not
 
 
 		with tf.name_scope("predict_hidden"):
-			with tf.device("/gpu:1"):
+			with tf.device("/cpu:1"):
 
 				h_predict_placeholder = tf.placeholder(shape=[self.hidden_size,1],dtype=tf.float32,name="h_predict")
 				c_predict_placeholder = tf.placeholder(shape=[self.hidden_size,1],dtype=tf.float32,name="c_predict")

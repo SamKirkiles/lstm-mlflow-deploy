2a951f55635172dbd5a99bc549ea36e29698edc6
SamKirkiles <sam.kirkiles@gmail.com>
2018-04-15 12:41:48 -0400

    fixed directory to checkpoints

--- lstm.py
+++ lstm.py
@@ -187,7 +187,7 @@ class LSTM:
 
 			# initialize all veraibles
 			if restore:
-  				saver.restore(sess, tf.train.latest_checkpoint('./saves/model.ckpt'))
+  				saver.restore(sess, tf.train.latest_checkpoint('./saves'))
 			else:
 				sess.run(tf.global_variables_initializer())
 
@@ -235,7 +235,7 @@ class LSTM:
 					print(out)
 
 
-					save_path = saver.save(sess, "./saves/model.ckpt",global_step=j)
+					save_path = saver.save(sess, "./saves",global_step=j)
 					print("Model saved in path: %s" % save_path)
 
 
